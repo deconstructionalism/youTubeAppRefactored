1e6751cdc046c48f40f369345172644949c99ca2
mckoyd <dominick.mckoy@gmail.com>
2018-04-10 18:04:53 -0400

    Added url and fetchVideos method to the Responses namespace and created a Store namespace with the videos array

--- Responses.js
+++ Responses.js
@@ -2,5 +2,14 @@
 // Create a namespace for all constants and methods
 // related to getting a response from an API.
 const Responses = (() => {
-    const API_KEY = 'AIzaSyBFz0lxQL-8arxYWSUs8rmCQSLM6jqAwrI';
+  const API_KEY = 'AIzaSyBFz0lxQL-8arxYWSUs8rmCQSLM6jqAwrI';
+  const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
+  const fetchVideos = (searchTerm, fn) => {
+    const query = {
+      q: `${searchTerm} in:name`,
+      key: API_KEY,
+      part: 'snippet'
+    };
+    $.getJSON(BASE_URL, query, fn);
+  };
 })();
