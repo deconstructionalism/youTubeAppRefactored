488c4e33013f4752effc0f5eefa45c7d4fbda1ac
mckoyd <dominick.mckoy@gmail.com>
2018-04-10 17:42:22 -0400

    Initial commit: created a namespace for API responses

---
+++ Responses.js
@@ -0,0 +1,6 @@
+'use strict';
+// Create a namespace for all constants and methods
+// related to getting a response from an API.
+const Responses = (() => {
+    const API_KEY = 'AIzaSyBFz0lxQL-8arxYWSUs8rmCQSLM6jqAwrI';
+})();
