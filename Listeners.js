bccb56e74d52cc39315d11e10adf04f1245240aa
mckoyd <dominick.mckoy@gmail.com>
2018-04-10 18:47:01 -0400

    Added handleFormSubmit to Listeners namespace

--- Listeners.js
+++ Listeners.js
@@ -1,10 +1,21 @@
 'use strict';
-/* global Results */
+/* global Results, Responses */
 const Listeners = (() => {
   const render = () => {
     $('.results').html(Results.videos.map(video => Results.generateVideoItemHtml(video)));
   };
+  const handleFormSubmit = () => {
+    $('form').submit(event => {
+      event.preventDefault();
+      const searchTerm = $('#search-term').val();
+      $('#search-term').val('');
+      Responses.fetchVideos(searchTerm, data => {
+        Results.addVideosToStore(Results.decorateResponse(data));
+        this.render();
+      });
+    });
+  };
   return {
-    render
+    render, handleFormSubmit
   };
 })();
